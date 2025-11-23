664f7750143fe68a7537e162f470b2fdc0951858
Pritam Pramanik <pritampramanik115@gmail.com>
2025-11-23 21:37:26 +0600

    Basic Project Setup

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './layout/Root.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+<RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
